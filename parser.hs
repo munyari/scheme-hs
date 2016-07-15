3c7aa824f1ab11cad8fd8bae35fb9cf9e983a1ec
Panashe M. Fundira <fundirap@gmail.com>
2016-07-15 16:25:29 -0400

    reformat parseCharacter

--- parser.hs
+++ parser.hs
@@ -65,22 +65,35 @@ parseCharacter :: Parser LispVal
 parseCharacter = parseNul <|> parsePage <|> parseReturn <|> parseRubout
                 <|> parseSpace <|> parseTab <|> parseAlarm <|> parseBackspace
                 <|> parseLinefeed <|> parseNewline <|> parseVtab
-                <|> parseEsc <|> parseDelete 
+                <|> parseEsc <|> parseDelete
                 <|> parseChar
                         where
-                          parseNul       = string("#\\nul") >> (return $ Character '\NUL')
-                          parseSpace     = string("#\\space") >> (return $ Character '\SP')
-                          parseReturn    = string("#\\return") >> (return $ Character '\CR')
-                          parsePage      = string("#\\page")  >> (return $ Character '\FF')
-                          parseTab       = string("#\\tab") >> (return $ Character '\BS')
-                          parseAlarm     = string("#\\alarm") >> (return $ Character '\BEL')
-                          parseBackspace = string("#\\backspace")  >> (return $ Character '\BS')
-                          parseLinefeed  = string("#\\linefeed") >> (return $ Character '\n')
-                          parseNewline   = string("#\\newline") >> (return $ Character '\r')
-                          parseVtab      = string("#\\vtab") >> (return $ Character '\VT')
-                          parseEsc       = string("#\\esc") >> (return $ Character '\ESC')
-                          parseDelete    = string("#\\delete") >> (return $ Character '\DEL')
-                          parseRubout    = string("#\\rubout") >> (return $ Character '\DEL')
+                          parseNul       = string("#\\nul")
+                              >> (return $ Character '\NUL')
+                          parseSpace     = string("#\\space")
+                              >> (return $ Character '\SP')
+                          parseReturn    = string("#\\return")
+                              >> (return $ Character '\CR')
+                          parsePage      = string("#\\page")
+                              >> (return $ Character '\FF')
+                          parseTab       = string("#\\tab")
+                              >> (return $ Character '\BS')
+                          parseAlarm     = string("#\\alarm")
+                              >> (return $ Character '\BEL')
+                          parseBackspace = string("#\\backspace")
+                              >> (return $ Character '\BS')
+                          parseLinefeed  = string("#\\linefeed")
+                              >> (return $ Character '\n')
+                          parseNewline   = string("#\\newline")
+                              >> (return $ Character '\r')
+                          parseVtab      = string("#\\vtab")
+                              >> (return $ Character '\VT')
+                          parseEsc       = string("#\\esc")
+                              >> (return $ Character '\ESC')
+                          parseDelete    = string("#\\delete")
+                              >> (return $ Character '\DEL')
+                          parseRubout    = string("#\\rubout")
+                              >> (return $ Character '\DEL')
                           parseChar      = do
                             string("\\#")
                             x1 <- symbol <|> letter <|> digit
