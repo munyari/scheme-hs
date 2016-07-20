a544df759013296b2459ddd8f95e6bdd597ca900
Panashe M. Fundira <fundirap@gmail.com>
2016-07-19 23:35:43 -0400

    chap 3: evaluation

--- parser.hs
+++ parser.hs
@@ -10,10 +10,10 @@ symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-                     Left err -> "No match: " ++ show err
-                     Right val -> "Found value " ++ show val
+                     Left err -> String $ "No match: " ++ show err
+                     Right val -> val
 
 data LispVal = Atom String
              | List [LispVal]
@@ -149,7 +149,37 @@ showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
 
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                           if null parsed
+                               then 0
+                               else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+
 main :: IO ()
-main = do
-  (expr:_) <- getArgs
-  putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head
